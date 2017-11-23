71f8802a8eb909d0ba83e3a60c49eb5fd20c4393
Pablo <pabloep96@gmail.com>
2017-11-23 00:58:11 -0600

    Initial Commit

---
+++ src/learningchess/LearningChess.java
@@ -0,0 +1,17 @@
+package learningchess;
+
+/**
+ *
+ * @author Pablo
+ */
+public class LearningChess {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new MainMenu();
+    }
+    
+}
